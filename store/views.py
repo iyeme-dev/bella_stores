f0d084564ab66eed7b483c99483bd7e4b8cba319
iyeme-dev <iyemeaji@yahoo.com>
2025-12-26 21:33:27 +0000

    Add function to add item to cart and update its quantity

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from .models import Category, Product, Cart, CartItem
 
 def home(request, category_slug=None):
     category_page = None
@@ -18,8 +18,35 @@ def productPage(request, category_slug, product_slug):
         raise e
     return render(request, 'product.html', {'product': product})
 
-def cart(request):
-    return render(request, 'cart.html')
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+        cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        if cart_item.quantity < cart_item.product.stock:
+            cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+        cart_item.save()
+
 
 
 
