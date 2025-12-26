c49d15a8c27aedb4ba8eabdfff8a0e4db4854f61
iyeme-dev <iyemeaji@yahoo.com>
2025-12-26 11:41:13 +0000

    Display products in homepage

--- store/views.py
+++ store/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-def home(request):
-    return render(request, 'home.html')
+def home(request, category_slug=None):
+    category_page = None
+    products_list = None
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products_list = Product.objects.filter(category=category_page, available=True)
+    else:
+        products_list = Product.objects.all().filter(available=True)
+    return render(request, 'home.html', {'category': category_page, 'products': products_list})
 
 def productPage(request):
     return render(request,'product.html')
 
-# Create your views here.
+
+
