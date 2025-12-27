a3df8e51a1c0354cff4701152e7c65e0c877b4bf
iyeme-dev <iyemeaji@yahoo.com>
2025-12-27 18:38:59 +0000

    Add a function to calculate the total number of items in the cart

--- store/views.py
+++ store/views.py
@@ -47,6 +47,16 @@ def add_cart(request, product_id):
         )
         cart_item.save()
 
+def cart_detail(request, total=0, counter=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            counter += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+
 
 
 
