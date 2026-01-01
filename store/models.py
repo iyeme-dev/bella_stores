a39f71dc2a8fca594f9acb418feb8fe16aae042a
iyeme-dev <iyemeaji@yahoo.com>
2026-01-01 15:16:57 +0000

    Create the order item model

--- store/models.py
+++ store/models.py
@@ -124,3 +124,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Price')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrderItem'
+
+    def sub_total(self):
+        return self.quantity * self.price
+
+    def __str__(self):
+        return self.product   
