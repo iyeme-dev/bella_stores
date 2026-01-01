d603e41c56a845f539ec6df31a2f3ab00bc38c86
iyeme-dev <iyemeaji@yahoo.com>
2026-01-01 15:12:37 +0000

    Create the order model

--- store/admin.py
+++ store/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from .models import Category, Product, Order, OrderItem, Review
-# Register your models here.
-
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
 
-
 admin.site.register(Category, CategoryAdmin)
-
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
--- store/models.py
+++ store/models.py
@@ -75,8 +75,6 @@ class Review(models.Model):
     
 
 # Model: Cart
-
-
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
@@ -103,3 +101,26 @@ class CartItem(models.Model):
 
     def __str__(self):
         return self.product
+    
+class Order(models.Model):
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Order Total')
+    emailAddress = models.EmailField(max_length=250, blank=True, verbose_name='Email Address')
+    created = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=250, blank=True)
+    billingCountry = models.CharField(max_length=250, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=250, blank=True)
+    shippingCountry = models.CharField(max_length=250, blank=True)
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.id)
