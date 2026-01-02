416c9679da792b1dfb72ded46b01ce7ff86e27c3
iyeme-dev <iyemeaji@yahoo.com>
2026-01-02 05:49:37 +0000

    Create order confirmation thank-you page

--- store/views.py
+++ store/views.py
@@ -150,6 +150,10 @@ def cart_remove_product(request, product_id):
     cart_item.delete()
     return redirect('cart_detail')
 
+def thanks_page(request, order_id): 
+    if order_id: 
+        customer_order = get_object_or_404(Order, id=order_id) 
+    return render(request, 'thankyou.html', {'customer_order': customer_order})
 
 
 
