ddf98f29eca45178a541f738f0949a890ae60c8b
iyeme-dev <iyemeaji@yahoo.com>
2026-01-07 21:35:31 +0000

    Add product images

--- bella_stores/settings.py
+++ bella_stores/settings.py
@@ -79,17 +79,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bella_stores.wsgi.application'
 
 # Database
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL", "")),
+    "sqlite": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
