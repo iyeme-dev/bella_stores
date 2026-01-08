7bee04b052d9848c1710f15683ec61b5998509fb
iyeme-dev <iyemeaji@yahoo.com>
2026-01-08 16:34:36 +0000

    Update static file settings

--- bella_stores/settings.py
+++ bella_stores/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
