45ab0771983957d756d5ff717e8322953ff6da94
iyeme-dev <iyemeaji@yahoo.com>
2026-01-16 00:54:33 +0000

    Fix anchor link error in footer

--- store/views.py
+++ store/views.py
@@ -168,7 +168,8 @@ def cart_remove_product(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     cart_item = CartItem.objects.get(product=product, cart=cart)
-    cart_item.delete()
+    cart_item.quantity -= 1
+    cart_item.save()
     return redirect('cart_detail')
 
 def thanks_page(request, order_id): 
