d1c4bfa145abc7b9d19c2df80d777608038b8b1a
iyeme-dev <iyemeaji@yahoo.com>
2026-01-16 03:02:56 +0000

    Updae footer links

--- store/tests/test_footer.py
+++ store/tests/test_footer.py
@@ -13,3 +13,13 @@ class FooterYearTests(TestCase):
         # Assert
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, f"© {current_year} Bella-Store")
+
+class FooterLinksTests(TestCase):
+    def test_footer_about_link_points_to_about_page(self):
+        # Act
+        resp = self.client.get(reverse("home"))
+        about_url = reverse("about")
+
+        # Assert
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, f'href="{about_url}"')
